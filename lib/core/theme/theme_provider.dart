a44869defded257cbe1cb0fc30ed04d11de29ba3
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-07-10 20:36:41 +0330

    add theme provider for changing

---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,21 @@
+//for change theme provider between light and dark
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  Brightness _brightness;
+  ThemeProvider(this._brightness);
+
+  Brightness get brightness => _brightness;
+
+  void updateBrightness(Brightness brightness) {
+    _brightness = brightness;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    _brightness = (_brightness == Brightness.dark) ? Brightness.light : Brightness.dark;
+    notifyListeners();
+  }
+
+
+}
