f719359b54dd1cc58005db2f70ec447185296a30
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-07-13 19:25:53 +0330

    add go to app button onboarding screen

--- lib/features/onboarding/presentation/onboarding_page.dart
+++ lib/features/onboarding/presentation/onboarding_page.dart
@@ -1,5 +1,7 @@
 import 'package:Hotelino/features/onboarding/presentation/onboarding_provider.dart';
+import 'package:Hotelino/features/onboarding/presentation/widget/onboarding_button.dart';
 import 'package:Hotelino/features/onboarding/presentation/widget/onboarding_item.dart';
+import 'package:Hotelino/routes/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,9 +20,11 @@ class _OnboardingPageState extends State<OnboardingPage> {
     //we just want get data not listen and use provider
     final onboardingProvider = Provider.of<OnboardingProvider>(context);
 
-    //get data 
+    //get data
     final onboardingData = onboardingProvider.onboardingData;
     final int totalPage = onboardingData.length;
+
+    final theme = Theme.of(context);
 
     return Scaffold(
       body: Column(
@@ -30,40 +34,116 @@ class _OnboardingPageState extends State<OnboardingPage> {
               controller: _pageController,
               itemCount: totalPage,
               onPageChanged: onboardingProvider.updateCurrentPage,
-              itemBuilder:  (context, index) {
+              itemBuilder: (context, index) {
                 final data = onboardingData[index];
-                return OnboardingItem(title: data["title"]!, description: data["description"]!, 
-                image: data["image"]!);
+                return OnboardingItem(
+                  title: data["title"]!,
+                  description: data["description"]!,
+                  image: data["image"]!,
+                );
               },
-            
             ),
           ),
-          const SizedBox(height: 20,),
-          buildPageIndicator(onboardingProvider.currentPage, totalPage, context),
-          const SizedBox(height: 20,)
+          const SizedBox(height: 20),
+          buildPageIndicator(
+            onboardingProvider.currentPage,
+            totalPage,
+            context,
+          ),
+          const SizedBox(height: 20),
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            OnboardingButton(
+              visible: onboardingProvider.currentPage > 0, 
+              icon: Icons.arrow_back, 
+              onPressed: () => _previousPage() , 
+              backgroundcolor: Colors.transparent, 
+              iconColor: theme.colorScheme.primary
+              ),
+               OnboardingButton(
+              visible: onboardingProvider.currentPage < totalPage -1, 
+              icon: Icons.arrow_forward, 
+              onPressed: () => _nextPage(), 
+              backgroundcolor: theme.colorScheme.primary, 
+              iconColor: Colors.white
+              )
+
+            ],
+
+          ),
+          ),
+          const SizedBox(height: 30,),
+          if(totalPage > 1) ...[
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return SizeTransition(
+                  child: child,
+                  sizeFactor: animation , 
+                  axisAlignment: -1,
+                  );
+              },
+              child: onboardingProvider.currentPage == totalPage -1
+              ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(onPressed: () {
+                    Navigator.pushReplacementNamed(context, AppRoute.home);
+                  }, child: const Text("شروع رزرو هتل ها")),
+                ),
+              )
+              : null
+            
+            )
+          ]
 
         ],
       ),
-
     );
   }
-  Widget buildPageIndicator(int currentIndex , int totalPage , BuildContext context ) {
+  
+  void _nextPage() {
+    final onboardingProvider = Provider.of<OnboardingProvider>(context , listen: false) ;
+    if(onboardingProvider.currentPage < onboardingProvider.onboardingData.length -1){
+      _pageController.nextPage(duration: Duration(milliseconds: 500), curve: Curves.ease);
+    }
+  }
+
+  void _previousPage(){
+    final onboardingProvider = Provider.of<OnboardingProvider>(context , listen: false);
+    if(onboardingProvider.currentPage > 0){
+      _pageController.previousPage(duration: Duration(milliseconds: 500), curve: Curves.ease);
+    }
+  }
+
+  Widget buildPageIndicator(
+    int currentIndex,
+    int totalPage,
+    BuildContext context,
+  ) {
     final theme = Theme.of(context);
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: 
-        List.generate(totalPage, (index) => AnimatedContainer(duration: Duration(microseconds: 300),
-        margin: EdgeInsets.symmetric(horizontal: 5),
-        width: currentIndex == index ? 12 :8,
-        height: currentIndex == index ? 12 : 8,
-        decoration: BoxDecoration(
-          color: currentIndex == index
-          ? theme.colorScheme.primary
-          : theme.colorScheme.primary.withValues(alpha: 0.3)
+      children: List.generate(
+        totalPage,
+        (index) => AnimatedContainer(
+          duration: const Duration(microseconds: 300),
+          margin: const EdgeInsets.symmetric(horizontal: 5),
+          width: currentIndex == index ? 12 : 8,
+          height: currentIndex == index ? 12 : 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: currentIndex == index
+                ? theme.colorScheme.primary
+                : theme.colorScheme.primary.withValues(alpha: 0.3),
+          ),
         ),
-        )
-        ,)
+      ),
     );
   }
 }
---
+++ lib/features/onboarding/presentation/widget/onboarding_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class OnboardingButton extends StatelessWidget {
+  final bool visible;
+  final IconData icon;
+  final VoidCallback onPressed;
+  final Color backgroundcolor;
+  final Color iconColor;
+  const OnboardingButton({
+    super.key,
+    required this.visible,
+    required this.icon,
+    required this.onPressed,
+    required this.backgroundcolor,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (child, animation) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+      child: visible
+          ? Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Theme.of(context).colorScheme.primary , width: 1)
+            ),
+              child: FloatingActionButton(
+                onPressed: onPressed,
+                backgroundColor: backgroundcolor,
+                shape: const CircleBorder(),
+                elevation: 0,
+                child: Icon(icon, color: iconColor),
+              ),
+            )
+          : const SizedBox(),
+    );
+  }
+}
