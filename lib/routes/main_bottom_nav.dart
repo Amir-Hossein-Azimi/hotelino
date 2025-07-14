fa4408063f17f7375252a0853df9d9affd58fef6
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-07-14 19:07:25 +0330

    edit persistenetTabView

--- lib/routes/main_bottom_nav.dart
+++ lib/routes/main_bottom_nav.dart
@@ -1,7 +1,9 @@
+import 'package:Hotelino/core/constants/constants.dart';
 import 'package:Hotelino/routes/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
 class MainBottomNav extends StatefulWidget {
   const MainBottomNav({super.key});
 
@@ -10,7 +12,7 @@ class MainBottomNav extends StatefulWidget {
 }
 
 class _MainBottomNavState extends State<MainBottomNav> {
-  //because set in run 
+  //because set in run
   late PersistentTabController _controller;
 
   @override
@@ -18,28 +20,92 @@ class _MainBottomNavState extends State<MainBottomNav> {
     super.initState();
     _controller = PersistentTabController(initialIndex: 0);
   }
-  _buildScreens(){
-    return [HomePage() , FavoritePage() , BookingPage() , ProfilePage()];
+
+  _buildScreens() {
+    return [HomePage(), FavoritePage(), BookingPage(), ProfilePage()];
   }
 
   List<PersistentBottomNavBarItem> _navBarsItems() {
     return [
       PersistentBottomNavBarItem(
-        icon: SvgPicture.asset("assets/images/nav_home.svg" , width: 20, height: 20, colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),),
-         
-        )
+        icon: SvgPicture.asset(
+          "assets/images/nav_home.svg",
+          width: 20,
+          height: 20,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+        activeColorPrimary: AppColors.primary,
+        inactiveIcon: SvgPicture.asset(
+          "assets/images/nav_home.svg",
+          width: 20,
+          height: 20,
+          colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(
+          "assets/images/nav_favorite.svg",
+          width: 20,
+          height: 20,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+        activeColorPrimary: AppColors.primary,
+        inactiveIcon: SvgPicture.asset(
+          "assets/images/nav_favorite.svg",
+          width: 20,
+          height: 20,
+          colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(
+          "assets/images/nav_booking.svg",
+          width: 20,
+          height: 20,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+        activeColorPrimary: AppColors.primary,
+        inactiveIcon: SvgPicture.asset(
+          "assets/images/nav_booking.svg",
+          width: 20,
+          height: 20,
+          colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: SvgPicture.asset(
+          "assets/images/nav_profile.svg",
+          width: 20,
+          height: 20,
+          colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+        activeColorPrimary: AppColors.primary,
+        inactiveIcon: SvgPicture.asset(
+          "assets/images/nav_profile.svg",
+          width: 20,
+          height: 20,
+          colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+        ),
+      ),
     ];
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return PersistentTabView(
-      context, 
+      context,
       controller: _controller,
-      onItemSelected: (value) {},
-      screens: []
-      );
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      navBarStyle: NavBarStyle.style7,
+      hideNavigationBarWhenKeyboardAppears: true,
+      stateManagement: true,
+      handleAndroidBackButtonPress: true,
+      confineToSafeArea: true,
+      animationSettings: NavBarAnimationSettings(
+        navBarItemAnimation: ItemAnimationSettings(duration: Duration(milliseconds: 300) , curve: Curves.ease)
+      ),
+    );
   }
 }
