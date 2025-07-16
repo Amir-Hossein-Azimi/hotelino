f6ab3d6d24e3434db1f63e8baf5d5d4b905c4191
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-07-16 17:18:45 +0330

    add HomePageDataConstants

---
+++ lib/features/home/data/models/homepage_data.dart
@@ -0,0 +1,18 @@
+//manage data in home page
+class HomePageData {
+  final List<String> favorites;
+  final List<String> discounted;
+  final List<String> recentlyViewed;
+  final List<String> popular;
+  final List<String> specialOffers;
+  final List<String> newest;
+
+  HomePageData({
+    required this.favorites,
+    required this.discounted,
+    required this.recentlyViewed,
+    required this.popular,
+    required this.specialOffers,
+    required this.newest, required List<String> favotires,
+  });
+}
