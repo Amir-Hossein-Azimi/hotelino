5829e679a4125e758bd7a082c121cd85c341e9c0
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-08-02 17:17:56 +0330

    add hotel_card

--- lib/features/home/presentation/widgets/hotel_card.dart
+++ lib/features/home/presentation/widgets/hotel_card.dart
@@ -1,3 +1,4 @@
+import 'package:Hotelino/core/utils/price_formatter.dart';
 import 'package:Hotelino/features/home/data/models/hotel.dart';
 import 'package:Hotelino/features/home/presentation/provider/favorite_item_provider.dart';
 import 'package:flutter/material.dart';
@@ -11,28 +12,87 @@ class HotelCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final favoriteProvider = Provider.of<FavoriteItemProvider>(context);
     final isFavorite = favoriteProvider.isFavorite(hotel.id);
-    return  SizedBox(
+    return SizedBox(
       width: 280,
       child: Card(
         elevation: 4,
-        margin: EdgeInsets.all(0),
+        margin: const EdgeInsets.all(0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Stack(
-              children: [],
+            Stack(children: []),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber, size: 20),
+                      const SizedBox(width: 4),
+                      Text(
+                        "${hotel.rating} (${formatPrice(hotel.reviewCount)})",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      Text(
+                        hotel.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const SizedBox(width: 8),
+                      Text(
+                        "${hotel.city}, ${hotel.country}",
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(width: 4),
+                      Icon(
+                        Icons.location_on,
+                        color: Theme.of(context).colorScheme.primary,
+                        size: 18,
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Text(
+                      "از ${formatPrice(hotel.pricePerNight)} / شب",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8 , left: 8),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                    
+                        child: const Text(
+                          "مشاهده و انتخاب اتاق",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8,),
+                ],
+              ),
             ),
-            Expanded(child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                SizedBox(height: 8,),
-                Icon(Icons.star , color: Colors.amber, size: 20,),
-                SizedBox(width: 4,),
-                Text("${hotel.rating} (${hotel.reviewCount})"),
-
-
-              ],
-            ))
           ],
         ),
       ),
