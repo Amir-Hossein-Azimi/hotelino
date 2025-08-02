889a9011509ee218635874828790b0ca542b4791
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-08-02 16:36:41 +0330

    add price formatter code

---
+++ lib/core/utils/price_formatter.dart
@@ -0,0 +1,7 @@
+//use INTL package
+import 'package:intl/intl.dart';
+
+String formatPrice(int price) {
+  final formatter = NumberFormat.decimalPattern('fa');
+  return formatter.format(price);
+}
--- lib/features/home/presentation/widgets/hotel_card.dart
+++ lib/features/home/presentation/widgets/hotel_card.dart
@@ -1,10 +1,41 @@
+import 'package:Hotelino/features/home/data/models/hotel.dart';
+import 'package:Hotelino/features/home/presentation/provider/favorite_item_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HotelCard extends StatelessWidget {
-  const HotelCard({super.key});
+  Hotel hotel;
+  HotelCard({super.key, required this.hotel});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final favoriteProvider = Provider.of<FavoriteItemProvider>(context);
+    final isFavorite = favoriteProvider.isFavorite(hotel.id);
+    return  SizedBox(
+      width: 280,
+      child: Card(
+        elevation: 4,
+        margin: EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [],
+            ),
+            Expanded(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(height: 8,),
+                Icon(Icons.star , color: Colors.amber, size: 20,),
+                SizedBox(width: 4,),
+                Text("${hotel.rating} (${hotel.reviewCount})"),
+
+
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
