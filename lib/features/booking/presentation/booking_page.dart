b7ca8347d2270686494835f391446fd2699b83b0
Amir-Hossein-Azimi <tweaterinestageram20@gmail.com>
2025-09-08 12:42:15 +0330

    use numberTextField in booking page

--- lib/features/booking/presentation/booking_page.dart
+++ lib/features/booking/presentation/booking_page.dart
@@ -1,6 +1,7 @@
 import 'package:Hotelino/features/booking/presentation/booking_provider.dart';
 import 'package:Hotelino/features/booking/presentation/widgets/booking_form_field.dart';
 import 'package:Hotelino/features/booking/presentation/widgets/date_picker_field.dart';
+import 'package:Hotelino/features/booking/presentation/widgets/number_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -110,7 +111,22 @@ class _BookingPageState extends State<BookingPage> {
                     }
                   },
                 ),
-
+                const SizedBox(height: 8,),
+                NumberFormField(
+                  initialValue: bookingProvider.booking.phoneNumber,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "لطفا شماره تلفن را درست وارد کنید";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      bookingProvider.setPhoneNumber(newValue);
+                    }
+                  },
+                ),
+                const SizedBox(height: 8,),
 
 
               ],
